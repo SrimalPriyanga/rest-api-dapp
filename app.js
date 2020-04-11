98f882cd127e35e4c8d735e374256925583f4ad8
Srimal Priyanga <s.priyanga22@gmail.com>
2020-04-11 20:36:11 +0530

    implementing http get

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const app = express();
+const bookRouter = express.Router();
 const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to my nodemon API');
 });
+
+bookRouter.route('/books').get((req,res) => {
+  const responce = { hello: 'This is my api' };
+  res.json(responce);
+})
+app.use('/api', bookRouter);
 
 app.listen(port, () => {
   console.log(`App listning on http://localhost:${port}`);
