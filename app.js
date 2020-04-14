0abee6d44f8e137a0364781854453248a30efef9
Srimal Priyanga <s.priyanga22@gmail.com>
2020-04-14 11:57:56 +0530

    create bookRouter and refactor app

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
+const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true, useUnifiedTopology: true });
 const port = process.env.port || 3000;
 const Book = require('./models/bookModel');
+const BookRouter = require('./routes/bookRouter')(Book);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api', BookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my nodemon API');
 });
-
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    console.log(book);
-    return res.json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
-app.use('/api', bookRouter);
 
 app.listen(port, () => {
   console.log(`App listning on http://localhost:${port}`);
